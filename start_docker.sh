ffdfe9760337e7de8109c290920f7e43eacd3bfd
Mihir Parikh <16520754+smartm13@users.noreply.github.com>
2021-05-26 14:40:13 +0530

    Enhance jupyter terminal experience
     - Changed default shell to bash (no need to always do bash -i at terminal start)
     - Mounted home directory for shell history persistence

--- start_docker.sh
+++ start_docker.sh
@@ -2,7 +2,8 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 docker rm jupyter-instance
 docker run -d --name jupyter-instance -p "7080-7090:7080-7090" -v $DIR/jupyter_entry_mount/:/home/mounted \
---entrypoint bash -e JUPYTER_PORT=7080 --restart=always --net=host jupyter-python \
+--entrypoint bash -e JUPYTER_PORT=7080 -e SHELL=/bin/bash -v $DIR/mounted_root:/root --restart=always \
+--net=host jupyter-python \
 -c "bash -c /home/mounted/envs/install_all.sh ; bash -c /home/mounted/envs/jupyter_launcher.sh"
 docker logs -f jupyter-instance
 
